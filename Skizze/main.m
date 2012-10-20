492ada941880f835b8b6e18ce8b0bd9c1648800c
Surasak Sermluxananon <surasaksermluxananon@Surasaks-MacBook-Pro.local>
2012-10-20 15:08:51 +0700

    Initial Commit

---
+++ Skizze/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Skizze
+//
+//  Created by Surasak Sermluxananon on 10/20/55 BE.
+//  Copyright __MyCompanyName__ 2555. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
